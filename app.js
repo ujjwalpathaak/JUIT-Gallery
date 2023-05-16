5e1461fe6f2d4ba5bf854d302915d8f2b890a665
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2023-05-16 12:39:20 +0530

    .

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const loginBtn = document.getElementById('login-btn');
+const loginBtn = document.getElementById("login-btn");
 
-loginBtn.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the form from submitting
+loginBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
 
-    const enrollment = document.getElementById('enrollment').value;
-    const password = document.getElementById('password').value;
+  const enrollment = document.getElementById("enrollment").value;
+  const password = document.getElementById("password").value;
 
-    // Make an HTTP request to the API
-    fetch(`https://juit-webkiosk-api.onrender.com/v1.0/login?enrollment=${enrollment}&password=${password}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                // If the API response is successful, log the user in
-                window.location.replace("D:/WebTech Project/JUIT-Gallery/home.html")
-                // alert('Login successful!');
-            } else {
-                // If the API response is unsuccessful, show an error message
-                alert('Invalid enrollment number or password');
-            }
-        })
-        .catch(error => {
-            // If there is an error with the API request, show an error message
-            alert('An error occurred while logging in');
-            console.error(error);
-        });
+  const url = "https://juit-webkiosk-api.onrender.com/v1.0/login";
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: enrollment,
+      password: password,
+    }),
+  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    if (result.success === true) {
+      window.location.replace("http://127.0.0.1:5500/home.html");
+    } else {
+    }
+  } catch (error) {
+    alert("Invalid Password or Enrollment Number");
+  }
 });
